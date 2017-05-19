9654dc7dcb515b6b204cf3a70c9b4ededfa6aa82
Eric <rwaberic@gmail.com>
2017-05-19 19:30:40 -0400

    FinancialApp 05192017

---
+++ FinancialApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinancialApp.Startup))]
+namespace FinancialApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
